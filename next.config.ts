5848833b90ca46cabc71a5a0d353f8c023f5d0ba
Anish Bokare <156392409+anishbokare@users.noreply.github.com>
2025-07-27 13:02:53 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -21,3 +21,17 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+const path = require('path');
+
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    appDir: true,
+  },
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    return config;
+  }
+};
+
+module.exports = nextConfig;
